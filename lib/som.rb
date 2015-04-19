7df2eec68967caa909a362c2aaf57a427d5172c9
Arvind Kushwah <arvindkushwah9@gmail.com>
2015-04-19 12:08:11 +0530

    first commit

---
+++ lib/som.rb
@@ -0,0 +1,9 @@
+require "som/version"
+
+module Som
+  # Your code goes here...
+  def self.process(str)
+    # TODO: process `str`
+    str
+  end
+end
---
+++ lib/som/version.rb
@@ -0,0 +1,3 @@
+module Som
+  VERSION = "0.1.0"
+end
